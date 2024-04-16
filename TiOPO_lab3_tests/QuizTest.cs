695d91791a2502ab0bf2e891279b5f254e955a3c
Sidenko M. E. <matvey.sidenko@mail.ru>
2024-04-16 18:09:09 +0700

    Добавлен тест для метода AskQuestion класса Quiz После успешного вывода вопроса должно уменьшится RemainedAnswersNumber Тест не пройден

--- TiOPO_lab3_tests/QuizTest.cs
+++ TiOPO_lab3_tests/QuizTest.cs
@@ -72,8 +72,15 @@ namespace TiOPO_lab3_tests
         {
             Quiz quiz = new Quiz() { Score = 0, Questions = [new Question() { }], RemainedAnswersNumber = 1 };
             quiz.AskQuestion();
-
             Assert.AreEqual(1, quiz.RemainedAnswersNumber);
+        }
+
+        [TestMethod]
+        public void AskQuestionMethod_WillChangeRemainedAnswersNumberAfterSuccessfullCall()
+        {
+            Quiz quiz = new Quiz() { Score = 0, Questions = [new Question() { CorrectAnswerNumber = 1, Answers = ["",""], SourceFileName="existed_file.txt" }], RemainedAnswersNumber = 1 };
+            quiz.AskQuestion();
+            Assert.AreEqual(0, quiz.RemainedAnswersNumber);
         }
 
         [TestMethod]
