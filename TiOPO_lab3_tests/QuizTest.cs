eb2745e9cd03efc15522df4e9d4e0bda00322204
Sidenko M. E. <matvey.sidenko@mail.ru>
2024-04-16 15:56:29 +0700

    Добавление теста на существование класса Quiz. Тест не пройден.

---
+++ TiOPO_lab3_tests/QuizTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiOPO_lab3_app;
+
+namespace TiOPO_lab3_tests
+{
+    [TestClass]
+    public class QuizTest
+    {
+        [TestMethod]
+        public void ClassExists()
+        {
+            Quiz quiz = new Quiz();
+            Assert.IsNotNull(quiz);
+        }
+    }
+}
